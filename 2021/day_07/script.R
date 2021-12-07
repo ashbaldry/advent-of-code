dc5293bafd3ff7c8ef1f10fa00128e07311024bf
Ashley Baldry <arbaldry91@gmail.com>
2021-12-07 07:51:56 +0000

    Solution to Part 2 - Day 7

--- 2021/day_07/script.R
+++ 2021/day_07/script.R
@@ -4,3 +4,7 @@ movements <- scan("2021/day_07/input.txt", what = numeric(), sep = ",", quiet = 
 min(vapply(seq(min(movements), max(movements)), \(x) sum(abs(movements - x)), numeric(1)))
 
 # Part 2
+# Part 2
+triangle_numbers <- sapply(seq(min(movements), max(movements)), \(x) sum(seq(x)))
+triangle_numbers[1] <- 0
+min(sapply(seq(min(movements), max(movements)), \(x) sum(triangle_numbers[abs(movements - x) + 1])))
