45a0f41e8eb934ec76bd4ab68a4f8aba0f4cb658
Ashley Baldry <arbaldry91@gmail.com>
2021-12-11 09:47:06 +0000

    Reducing longest bottleneck

--- 2021/day_11/script.R
+++ 2021/day_11/script.R
@@ -5,20 +5,21 @@ octopi <- as.matrix(octopi)
 countNFlashes <- function(octopi, n = 100, i = 1, flashes = 0) {
   octopi <- octopi + 1
 
-  if (any(octopi > 9)) {
-    new_flashing_octopi <- flashing_octopi <- octopi > 9
-    while (any(new_flashing_octopi)) {
-      updating_octopi <- getSurroundingOctopi(which(new_flashing_octopi, arr.ind = TRUE))
-      for (j in seq_along(updating_octopi)) {
-        octopi[updating_octopi[[j]]] <- octopi[updating_octopi[[j]]] + 1
-      }
-      new_flashing_octopi <- octopi > 9 & !flashing_octopi
-      flashing_octopi <- octopi > 9
+  new_flashing_octopi <- flashing_octopi <- octopi > 9
+  while (any(new_flashing_octopi)) {
+    new_locations <- which(new_flashing_octopi, arr.ind = TRUE)
+    updating_octopi <- apply(new_locations, 1, findSurroundingOctopi, simplify = FALSE)
+
+    for (j in seq_along(updating_octopi)) {
+      octopi[updating_octopi[[j]]] <- octopi[updating_octopi[[j]]] + 1
     }
 
-    flashes <- flashes + sum(octopi > 9)
-    octopi[flashing_octopi] <- 0
+    new_flashing_octopi <- octopi > 9 & !flashing_octopi
+    flashing_octopi <- octopi > 9
   }
+
+  flashes <- flashes + sum(octopi > 9)
+  octopi[flashing_octopi] <- 0
 
   if (i == n) {
     flashes
@@ -27,12 +28,12 @@ countNFlashes <- function(octopi, n = 100, i = 1, flashes = 0) {
   }
 }
 
-getSurroundingOctopi <- function(locations) {
+findSurroundingOctopi <- function(x) {
   # Getting box of locations
   boxLocations <- \(x) max(1, x - 1):min(10, x + 1)
-  all_locations <- apply(
-    locations, 1, \(x) as.matrix(merge(boxLocations(x[1]), boxLocations(x[2]))), simplify = FALSE
-  )
+  rows <- boxLocations(x[1])
+  cols <- boxLocations(x[2])
+  cbind(row = rep(rows, times = length(cols)), col = rep(cols, each = length(rows)))
 }
 
 countNFlashes(octopi)
@@ -41,19 +42,20 @@ countNFlashes(octopi)
 countAllFlashes <- function(octopi, i = 1) {
   octopi <- octopi + 1
 
-  if (any(octopi > 9)) {
-    new_flashing_octopi <- flashing_octopi <- octopi > 9
-    while (any(new_flashing_octopi)) {
-      updating_octopi <- getSurroundingOctopi(which(new_flashing_octopi, arr.ind = TRUE))
-      for (j in seq_along(updating_octopi)) {
-        octopi[updating_octopi[[j]]] <- octopi[updating_octopi[[j]]] + 1
-      }
-      new_flashing_octopi <- octopi > 9 & !flashing_octopi
-      flashing_octopi <- octopi > 9
+  new_flashing_octopi <- flashing_octopi <- octopi > 9
+  while (any(new_flashing_octopi)) {
+    new_locations <- which(new_flashing_octopi, arr.ind = TRUE)
+    updating_octopi <- apply(new_locations, 1, findSurroundingOctopi, simplify = FALSE)
+
+    for (j in seq_along(updating_octopi)) {
+      octopi[updating_octopi[[j]]] <- octopi[updating_octopi[[j]]] + 1
     }
 
-    octopi[flashing_octopi] <- 0
+    new_flashing_octopi <- octopi > 9 & !flashing_octopi
+    flashing_octopi <- octopi > 9
   }
+
+  octopi[flashing_octopi] <- 0
 
   if (all(flashing_octopi)) {
     i
