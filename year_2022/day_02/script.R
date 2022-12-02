45cf5d2422feae60e8fcf85cb068ffb1815542ec
Ashley Baldry <arbaldry91@gmail.com>
2022-12-02 07:17:01 +0000

    Post solution to Part 2

--- year_2022/day_02/script.R
+++ year_2022/day_02/script.R
@@ -4,22 +4,15 @@ rps$elf_rps <- with(rps, match(elf_play, LETTERS[1:3]))
 rps$my_rps <- with(rps, match(my_play, LETTERS[24:26]))
 
 # Part 1
-rps$my_outcome <- with(rps, my_rps - elf_rps)
+rps$my_outcome <- with(rps, (my_rps %% 3) - (elf_rps %% 3))
 rps$my_outcome <- with(rps, ifelse(my_outcome == 2, -1, ifelse(my_outcome == -2, 1, my_outcome)))
 rps$my_score <- with(rps, my_rps + 3 * (1 + my_outcome))
 sum(rps$my_score)
 
-# Part 1 - Cleaned
-rps$my_outcome <- with(rps, (1 + my_rps - elf_rps) %% 3)
-rps$my_score <- with(rps, my_rps + 3 * my_outcome)
-sum(rps$my_score)
-
 # Part 2
-rps$my_rps_2 <- with(
-  rps,
-  ifelse(
-    my_play == "X",
-    ifelse(elf_rps == 1, 3, elf_rps - 1),
+rps$my_rps_2 <- with(rps, ifelse(
+  my_play == "X",
+  ifelse(elf_rps == 1, 3, elf_rps - 1),
   ifelse(
     my_play == "Y",
     elf_rps,
@@ -28,6 +21,11 @@ rps$my_rps_2 <- with(
 ))
 rps$my_score_2 <- with(rps, my_rps_2 + 3 * (my_rps - 1))
 sum(rps$my_score_2)
+
+# Part 1 - Cleaned
+rps$my_outcome <- with(rps, (1 + my_rps - elf_rps) %% 3)
+rps$my_score <- with(rps, my_rps + 3 * my_outcome)
+sum(rps$my_score)
 
 # Part 2 - Cleaned
 rps$my_rps_2 <- with(rps, (elf_rps + my_rps - 2 - 1) %% 3 + 1)
