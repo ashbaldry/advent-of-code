99e6a42db72116d34ece1a7ccfe4d2abddd995a4
Ashley Baldry <arbaldry91@gmail.com>
2022-12-04 10:21:25 +0000

    Show solution to Day 4 2022

--- year_2022/day_04/script.R
+++ year_2022/day_04/script.R
@@ -5,6 +5,8 @@ assignments$A2 <- with(assignments, as.numeric(sub(".*-", "", A)))
 assignments$B1 <- with(assignments, as.numeric(sub("-.*", "", B)))
 assignments$B2 <- with(assignments, as.numeric(sub(".*-", "", B)))
 
+assignments <- read.table(text = gsub("-", ",", readLines("input.txt")), sep = ",", col.names = c("A1", "A2", "B1", "B2"))
+
 #### Part 1 ####
 with(assignments, sum((A1 >= B1 & A2 <= B2) | (B1 >= A1 & B2 <= A2)))
 
